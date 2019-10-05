61f995342d47e0f87a80fee2603386461b9ca7a1
Alvaro Palacios Carrillo <apalacioscarrillo@gmail.com>
2019-10-04 23:57:10 -0500

    ADD: Little navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
+import appLogo from "./images/logo.jpg";
+import "./styles/Navbar.css";
 
 function App() {
   return (
     <div>
       <header>
-
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand">
+            <img className="img width-logo" src={appLogo} alt="Logo" />
+          </a>
+        </nav>
       </header>
     </div>
   );
