94859dab8d0507c2117c02c90c5d1e931c27bc9b
Alvaro Palacios Carrillo <apalacioscarrillo@gmail.com>
2019-10-07 18:45:41 -0500

    REFACT: Home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,13 +5,40 @@ import "../styles/Navbar.css";
 class Navbar extends React.Component {
   render() {
     return (
-      <header>
-        <nav className="navbar navbar-light navbar-color">
-          <a className="navbar-brand">
-            <img className="img width-logo" src={appLogo} alt="Logo" />
-          </a>
-        </nav>
-      </header>
+      <nav class="navbar navbar-expand-lg navbar-color p-0">
+        <a class="navbar-brand" href="/">
+          <img src={appLogo} className="width-logo" />
+        </a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#!">
+                Acerca de
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#!">
+                Blog
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#!">
+                Gallería
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#!">
+                Contacto
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link toggle-search" href="#!">
+                <i class="fa fa-search"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,91 @@
 import React, { Component } from "react";
+import "../styles/Home.css";
 
 export default class Home extends Component {
   render() {
-    return <h1>este es el home</h1>;
+    return (
+      <section id="hero">
+        <div class="hero-heading">
+          <h1>We like to create awesome designs.</h1>
+          <p>
+            Sunlimetech is an advanced and proactive at the app development and
+            providing you all the ultimate software development services that
+            you need. Sunlimetech ensure to offer you all the required &
+            ultimate services meeting the advanced deadlines and requirements of
+            the field. We believe in providing the ultimate services with
+            quality and meeting all the deadlines, application development
+            industry is at its peak and hitting the new dimensions rapidly.
+          </p>
+        </div>
+        <div class="row mgmt-links-wrapper">
+          <div class="col-sm-6 col-md-4 col-lg-2">
+            <a
+              href="https://www.fiverr.com/sunlimetech/design-and-fix-your-bootstrap-4-issues"
+              target="_blank"
+            >
+              <div class="link-box">
+                <i class="fa fa-heart"></i>
+                <p>Visual Design</p>
+              </div>
+            </a>
+          </div>
+          <div class="col-sm-6 col-md-4 col-lg-2">
+            <a
+              href="https://www.fiverr.com/sunlimetech/fix-your-bootstrap-html-and-css3-issues"
+              target="_blank"
+            >
+              <div class="link-box">
+                <i class="fa fa-wrench"></i>
+                <p>Development</p>
+              </div>
+            </a>
+          </div>
+          <div class="col-sm-6 col-md-4 col-lg-2">
+            <a
+              href="https://www.fiverr.com/sunlimetech/do-web-development-in-pure-php-and-frameworks"
+              target="_blank"
+            >
+              <div class="link-box">
+                <i class="fa fa-users"></i>
+                <p>Management</p>
+              </div>
+            </a>
+          </div>
+          <div class="col-sm-6 col-md-4 col-lg-2">
+            <a
+              href="https://www.fiverr.com/sunlimetech/provide-responsive-design-for-globally-existing-devices"
+              target="_blank"
+            >
+              <div class="link-box">
+                <i class="fa fa-share-alt"></i>
+                <p>Marketing</p>
+              </div>
+            </a>
+          </div>
+          <div class="col-sm-6 col-md-4 col-lg-2">
+            <a
+              href="https://www.fiverr.com/sunlimetech/convert-bootstrap-or-html-to-wordpress-for-global-devices"
+              target="_blank"
+            >
+              <div class="link-box">
+                <i class="fa fa-cart-plus"></i>
+                <p>eCommerce</p>
+              </div>
+            </a>
+          </div>
+          <div class="col-sm-6 col-md-4 col-lg-2">
+            <a
+              href="https://www.fiverr.com/sunlimetech/optimize-your-website-speed-and-performance-via-speedinsight"
+              target="_blank"
+            >
+              <div class="link-box">
+                <i class="fa fa-eye"></i>
+                <p>Branding</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </section>
+    );
   }
 }
